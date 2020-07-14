5ccd7db965690f6fd0a8299690c73a728242babd
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-07-14 13:45:28 +0900

    #6.3 Preloading fonts

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, Image } from "react-native";
+import Gate from "./components/Gate";
+
+const cacheImages = (images) =>
+    images.map((image) => {
+        if (typeof image === "string") {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        }
+    });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Welcome to Airbnb</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    const [isReady, setIsReady] = useState(false);
+    const handleFinish = () => setIsReady(true);
+    const loadAssets = async () => {
+        const images = [require("./assets/loginBg.jpeg"), "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png"];
+        const fonts = [Ionicons.font];
+        const imagePromises = cacheImages(images);
+        const fontPromises = cacheFonts(fonts);
+        return Promise.all([...fontPromises, ...imagePromises]);
+    };
+    return isReady ? <Gate /> : <AppLoading onError={console.error} onFinish={handleFinish} startAsync={loadAssets} />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
---
+++ README.md
@@ -0,0 +1,16 @@
+# Aribnb React Native App
+
+# 6 App Setup
+
+## 6.1 Setup
+
+-   yarn gloabl add expo-cli
+-   expo init APPNAME
+
+## 6.2 Preloading Images
+
+-   yarn add expo-asset
+
+## 6.3 Preloading Font
+
+-   expo add expo-0font
