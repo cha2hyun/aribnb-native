ddcb769ce77ab93196adb20c741aed6edc947f2f
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-14 18:47:37 +0900

    #8.3 Welcome Screen Part Two

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import { StatusBar } from "react-native";
 import { BlurView } from "expo-blur";
+import Btn from "../components/Auth/Btn";
 
 const LOGO_URL = "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png";
 const Container = styled.View`
@@ -19,11 +20,19 @@ const Logo = styled.Image`
     height: 100px;
 `;
 
+const BtnContainer = styled.View``;
+
 export default ({ navigation }) => {
+    const goToSignUp = () => navigation.navigate("SignUp");
+    const goToSignIn = () => navigation.navigate("SignIn");
     return (
         <Container>
             <BlurView intensity={30} tint="light" style={{ flex: 1, width: "100%", alignItems: "center", justifyContent: "center" }}>
                 <Logo source={{ uri: LOGO_URL }} />
+                <BtnContainer>
+                    <Btn onPress={goToSignUp} text={"Sign Up"} accent={true} />
+                    <Btn onPress={goToSignIn} text={"Sign In"} />
+                </BtnContainer>
             </BlurView>
             <Image resizeMethod="auto" source={require("../assets/loginBg.jpeg")} />
             <StatusBar barStyle="light-content" />
