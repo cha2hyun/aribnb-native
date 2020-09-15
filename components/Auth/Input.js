56ae524bf2baa384b63e1ebd9c3c5e548abb4a44
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-15 11:17:50 +0900

    #8.6 bugfixing (keyboarddismiss)

--- components/Auth/Input.js
+++ components/Auth/Input.js
@@ -2,11 +2,18 @@ import React from "react";
 import { TouchableOpacity, Dimensions } from "react-native";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
+import colors from "../../colors";
 
 const { width } = Dimensions.get("screen");
 
 const Container = styled.TextInput`
     width: ${width / 1.5}px;
+    padding: 12.5px 20px;
+    border: 1px solid gray;
+    background-color: white;
+    border-radius: 30px;
+    margin-bottom: 10px;
+    font-weight: 500;
 `;
 
 const Input = ({ value, placeholder, isPassword = false, autoCapitalize, stateFn }) => <Container value={value} placeholder={placeholder} secureTextEntry={isPassword ? true : false} autoCapitalize={autoCapitalize} onChangeText={(text) => stateFn(text)} />;
--- screens/Auth/SignUp.js
+++ screens/Auth/SignUp.js
@@ -1,8 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Keyboard, KeyboardAvoidingView, StatusBar, TouchableWithoutFeedback } from "react-native";
+import styled from "styled-components/native";
+import Btn from "../../components/Auth/Btn";
+import Input from "../../components/Auth/Input";
+import DismissKeyboard from "../../components/DismissKeyboard";
 
-export default () => (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-        <Text>Sign Up</Text>
-    </View>
-);
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const InputContainer = styled.View`
+    margin-bottom: 30px;
+`;
+
+export default () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = () => {
+        alert(`${username}, ${password}`);
+    };
+    const dismissKeybooard = () => Keyboard.dismiss();
+
+    return (
+        <DismissKeyboard>
+            <Container>
+                <StatusBar barStyle="dark-content"></StatusBar>
+                <KeyboardAvoidingView behavior="position">
+                    <InputContainer>
+                        <Input value={firstName} placeholder="First Name" autoCapitalize="none" stateFn={setFirstName} />
+                        <Input value={lastName} placeholder="Last Name" autoCapitalize="none" stateFn={setLastName} />
+                        <Input value={username} placeholder="Username" autoCapitalize="none" stateFn={setUsername} />
+                        <Input value={password} placeholder="Password" isPassword={true} stateFn={setPassword} />
+                    </InputContainer>
+                    <Btn text={"Sign Up"} accent onPress={handleSubmit}></Btn>
+                </KeyboardAvoidingView>
+            </Container>
+        </DismissKeyboard>
+    );
+};
