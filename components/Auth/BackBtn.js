ecbe963e7b3fc061db863e939720c284e4ca7a73
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-21 14:08:39 +0900

    #9.1 Main Navigation part Two

--- components/Auth/BackBtn.js
+++ components/Auth/BackBtn.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Platform } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 
-const isAndroid = Platform.OS === "android";
+import { Ionicons } from "@expo/vector-icons";
+import { isAndroid } from "../../utils";
+
 const Container = styled.View`
     padding-left: 20px;
 `;
 
 export default () => (
     <Container>
-        <Ionicons name={isAndroid ? "md-arrow-down" : "ios-arrow-down"} size={28} />
+        <Ionicons name={utils.isAndroid() ? "md-arrow-down" : "ios-arrow-down"} size={28} />
     </Container>
 );
