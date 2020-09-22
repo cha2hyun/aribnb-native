b613880ce38192c8e9cd717dc7aae56cb321f3c5
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-22 14:09:33 +0900

    #10.2 Toggle Favs part two (saved fav rooms in redux)

--- redux/usersSlice.js
+++ redux/usersSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import api from "../api";
+import { setFavs } from "./roomsSlice";
 
 const userSlice = createSlice({
     name: "users",
@@ -41,9 +42,21 @@ export const getFavs = () => async (dispatch, getState) => {
     } = getState();
     try {
         const { data } = await api.favs(id);
-        console.log(data);
+        dispatch(setFavs(data));
     } catch (e) {
-        alert("Error");
+        console.warn(e);
+    }
+};
+
+export const toggleFav = (roomId) => async (dispatch, getState) => {
+    const {
+        usersReducer: { id, token },
+    } = getState();
+    try {
+        const { status } = await api.toggleFavs(id, roomId, token);
+        console.log(status);
+    } catch (e) {
+        console.warn(e);
     }
 };
 
