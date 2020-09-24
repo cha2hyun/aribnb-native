f7bf8e19bb140746ff0f5763747f502da3ee62ea
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-24 15:22:35 +0900

    #13.1 Map View Cards

--- screens/Main/Map.js
+++ screens/Main/Map.js
@@ -1,10 +1,10 @@
 import React from "react";
-import MapView from "react-native-maps";
-import { connect } from "react-redux";
 import styled from "styled-components/native";
-import { Dimensions, StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+import { connect } from "react-redux";
+import MapView from "react-native-maps";
 
-const { width, heighht } = Dimensions.get("screen");
+const { width, height } = Dimensions.get("screen");
 
 const Container = styled.View`
     flex: 1;
@@ -14,8 +14,9 @@ const Container = styled.View`
 
 const ScrollView = styled.ScrollView`
     position: absolute;
-    bottom: 50;
+    bottom: 50px;
 `;
+
 const RoomContainer = styled.View`
     background-color: transparent;
     width: ${width}px;
@@ -24,26 +25,66 @@ const RoomContainer = styled.View`
 
 const RoomCard = styled.View`
     background-color: white;
-    width: ${width - 50}px;
-    height: 200px;
+    width: ${width - 80}px;
+    height: 120px;
+    border-radius: 10px;
+    padding: 0px 20px;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const RoomPhoto = styled.Image`
+    width: 80px;
+    height: 80px;
+    border-radius: 5px;
     margin-right: 20px;
 `;
-const RoomName = styled.Text``;
 
-const Map = ({ rooms }) => (
-    <Container>
-        <MapView style={StyleSheet.absoluteFill} />
-        <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled horizontal>
-            {rooms?.map((room) => (
-                <RoomContainer key={room.id}>
-                    <RoomCard>
-                        <RoomName>{room.name}</RoomName>
-                    </RoomCard>
-                </RoomContainer>
-            ))}
-        </ScrollView>
-    </Container>
-);
+const Column = styled.View`
+    width: 70%;
+`;
+
+const RoomName = styled.Text`
+    font-size: 18px;
+`;
+
+const RoomPrice = styled.Text`
+    margin-top: 5px;
+    font-size: 16px;
+`;
+
+const Map = ({ rooms }) => {
+    return (
+        <Container>
+            <MapView
+                style={StyleSheet.absoluteFill}
+                camera={{
+                    center: {
+                        latitude: parseFloat(rooms[0].lat),
+                        longitude: parseFloat(rooms[0].lng),
+                    },
+                    altitude: 700,
+                    pitch: 0,
+                    heading: 0,
+                    zoom: 10,
+                }}
+            />
+            <ScrollView showsHorizontalScrollIndicator={false} pagingEnabled horizontal>
+                {rooms?.map((room) => (
+                    <RoomContainer key={room.id}>
+                        <RoomCard>
+                            <RoomPhoto source={room.photos[0]?.file ? { uri: room.photos[0]?.file } : require("../../assets/roomDefault.jpeg")} />
+                            <Column>
+                                <RoomName>{room.name}</RoomName>
+                                <RoomPrice>${room.price}</RoomPrice>
+                            </Column>
+                        </RoomCard>
+                    </RoomContainer>
+                ))}
+            </ScrollView>
+        </Container>
+    );
+};
 
 function mapStateToProps(state) {
     return { rooms: state.roomsReducer.explore.rooms };
